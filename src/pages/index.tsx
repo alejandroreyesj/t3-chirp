b5344f9db8be85cdb09eb7a61b7d90533dd36495
Alejandro R <alejandro.reyesj@outlook.com>
2023-03-24 01:23:57 -0400

    Added Posts Table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { api } from "~/utils/api";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.example.getAll.useQuery();
   const user = useUser();
   return (
     <>
@@ -16,8 +16,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((item) => (
+            <div key={item.id}>{item.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
